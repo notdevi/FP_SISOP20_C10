3e4cb7b865fac4dbf7d4117abcc8b85091957aa5
anggarayp <61231385+anggarayp@users.noreply.github.com>
2020-05-08 17:03:40 +0700

    Update head.c

--- head.c
+++ head.c
@@ -10,58 +10,50 @@ void print_help() {
 	exit();
 }
 
-void error_head(uint err, char* s) {
-	switch(err) {
-		case(0) :
-			printf(2, "head: syntax error\n");
-			break;
-		case(1) :
-			printf(2, "head: invalid options %s\nTry 'head --help' for more information\n", s);
-			break;
-		case(2) :
-			printf(2, "head: syntax error, expected at least one argument\n");
-			break;		
-		case(3) :
-			printf(2, "head: nvalid number of lines: %s\n", s);
-			break;
-		case(4) :
-			printf(2, "head: invalid number of bytes: %s\n", s);
-			break;	
-		case(5) :
-			printf(2, "head: read error\n");
-			break;
-		case(6) :
-			printf(2, "head: cannot open '%s' for reading: No such file or directory\n", s);
-			break;
-		default:
-			printf(2, "head: error\n");
+void errors(uint err, char* str) {
+	if(err == 0) {
+		printf(2, "head: syntax error\n");
+	} else if(err == 1) {
+		printf(2, "head: invalid options %s\nTry 'head --help' for more information\n", str);
+	} else if(err == 2) {
+		printf(2, "head: syntax error, expected at least one argument\n");
+	} else if(err == 3) {
+		printf(2, "head: nvalid number of lines: %s\n", str);
+	} else if(err == 4) {
+		printf(2, "head: invalid number of bytes: %s\n", str);
+	} else if(err == 5) {
+		printf(2, "head: read error\n");
+	} else if(err == 6) {
+		printf(2, "head: cannot open '%s' for reading: No such file or directory\n", str);
+	} else {
+		printf(2, "head: error\n");
 	}
 	exit();
 }
 
-void head(char *fn, int n, int mode, int moden) {
+void head(char *file_name, int n, int mode, int mode_n) {
 	int r, fd, l = 0;
 	
-	if(strcmp(fn, STDIN) == 0) {
+	if(strcmp(file_name, STDIN) == 0) {
 		fd = 0;
 	} else {
-		fd = open(fn, 0);
+		fd = open(file_name, 0);
 		if(fd < 0) {
-			error_head(6, fn);
+			errors(6, file_name);
 		}
 	}
 
 	if(mode == 2) {
-		printf(1, "filename : %s\n", fn);
+		printf(1, "filename : %s\n", file_name);
 	}
 
 	while((r = read(fd, buff, sizeof(buff))) > 0 && l < n) {
 		for(int i = 0; i < r && l < n; i++) {
 			printf(1, "%c", buff[i]);
 
-			if(buff[i] == '\n' && moden != 2) {
+			if(buff[i] == '\n' && mode_n != 2) {
 				l++;
-			} else if(moden == 2) {
+			} else if(mode_n == 2) {
 				l++;
 			}
 		}
@@ -69,62 +61,56 @@ void head(char *fn, int n, int mode, int moden) {
 	
 	if (r < 0) {
 		close(fd);
-		error_head(5, 0);
+		errors(5, 0);
 	}
 	close(fd);
 }
 
 int main(int argc, char *argv[]) {
-	int i, mode = 0, moden = 0, n = 10;
+	int i, mode = 0, mode_n = 0, n = 10;
 	char *c;
 
 	if(argc <= 1) {
-		head(STDIN, n, mode, moden);
+		head(STDIN, n, mode, mode_n);
 		exit();
 	}
 
 	for(i = 1; i < argc; i++) {
 		strcpy(buff, argv[i]);
 		if(buff[0] == '-') {
-			switch(buff[1]) {
-				case 'n' :
-					if(atoi(argv[++i]) == 0) {
-						error_head(3, argv[i]);
-					}
-					n = atoi(argv[i]);
-					moden = 1;
-					break;
-				case 'c' :
-					if(atoi(argv[++i]) == 0) {
-						error_head(4, argv[i]);
-					}
-					n = atoi(argv[i]);
-					moden = 2;
-					break;
-				case 'v' :
-					mode = 2;
-					break;
-				case 'q' :
-					mode = 1;
-					break; 
-				case '-' :
-					c = buff;
-					c = c + 2;
-					if(strcmp(c, "help") == 0) {
-						print_help();
-					} else {
-						error_head(1, argv[i]);
-					}
-					break;
-				default :
-					error_head(1, argv[i]);
-					break;
+			if(buff[1] == 'n') {
+				if(atoi(argv[++i]) == 0) {
+					errors(3, argv[i]);
+				}
+				n = atoi(argv[i]);
+				mode_n = 1;
+			} else if(buff[1] == 'c') {
+				if(atoi(argv[++i]) == 0) {
+					errors(4, argv[i]);
+				}
+				n = atoi(argv[i]);
+				mode_n = 2;
+			} else if(buff[1] == 'v') {
+				mode = 2;
+			} else if(buff[1] == 'q') {
+				n = 20;
+				mode = 1; 
+			} else if(buff[1] == '-') {
+				c = buff;
+				c = c + 2;
+				if(strcmp(c, "help") == 0) {
+					print_help();
+				} else {
+					errors(1, argv[i]);
+				}
+			} else {
+				errors(1, argv[i]);
 			}
 		} else {
 			if(i+1 < argc && mode == 0) {
 				mode = 2;
 			}
-			head(argv[i], n, mode, moden);
+			head(argv[i], n, mode, mode_n);
 			if(i+1 < argc) {
 				printf(1, "\n");
 			}
@@ -133,4 +119,3 @@ int main(int argc, char *argv[]) {
 	exit();
 }
 
-				

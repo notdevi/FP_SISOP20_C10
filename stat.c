c3c22f0c69ead4e1ed4a78e10619e06963293159
anggarayp <61231385+anggarayp@users.noreply.github.com>
2020-05-08 17:46:24 +0700

    Update stat.c

--- stat.c
+++ stat.c
@@ -3,11 +3,43 @@
 #include "user.h"
 #include "fs.h"
 
+struct stat stt;
+
+void info(char *path) {
+  char *nama = path;
+  if(stt.type == T_DIR){
+      printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
+      printf(1, "\n");
+      printf(1, "Size\t\t: %d\n", stt.size);
+      printf(1, "Type\t\t: Directory\n");
+      printf(1, "Device\t\t: %d\n", stt.dev);
+      printf(1, "File Inode\t: %d\n", stt.ino);
+      printf(1, "Links\t\t: %d\n", stt.nlink);
+    }
+  else if(stt.type == T_FILE){
+      printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
+      printf(1, "\n");
+      printf(1, "Size\t\t: %d\n", stt.size);
+      printf(1, "Type\t\t: File\n");
+      printf(1, "Device\t\t: %d\n", stt.dev);
+      printf(1, "File Inode\t: %d\n", stt.ino);
+      printf(1, "Links\t\t: %d\n", stt.nlink);
+  }
+  else if(stt.type == T_DEV){
+      printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
+      printf(1, "\n");
+      printf(1, "Size\t\t: %d\n", stt.size);
+      printf(1, "Type\t\t: Device\n");
+      printf(1, "Device\t\t: %d\n", stt.dev);
+      printf(1, "File Inode\t: %d\n", stt.ino);
+      printf(1, "Links\t\t: %d\n", stt.nlink);
+    }
+}
+
 int main(int argc, char *argv[]){
-  struct stat stt;
-  int fd;
+  int fd, i;
   char *nama = argv[1];
-  
+
   if(argc < 2){
     printf(1, "Missing File Operand\n");
   }
@@ -21,32 +53,8 @@ int main(int argc, char *argv[]){
     close(fd);
   }
   
-  if(stt.type == T_DIR){
-    printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
-    printf(1, "\n");
-    printf(1, "Size\t\t: %d\n", stt.size);
-    printf(1, "Type\t\t: Directory\n");
-    printf(1, "Device\t\t: %d\n", stt.dev);
-    printf(1, "File Inode\t: %d\n", stt.ino);
-    printf(1, "Links\t\t: %d\n", stt.nlink);
-  }
-  else if(stt.type == T_FILE){
-    printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
-    printf(1, "\n");
-    printf(1, "Size\t\t: %d\n", stt.size);
-    printf(1, "Type\t\t: File\n");
-    printf(1, "Device\t\t: %d\n", stt.dev);
-    printf(1, "File Inode\t: %d\n", stt.ino);
-    printf(1, "Links\t\t: %d\n", stt.nlink);
-  }
-  else if(stt.type == T_DEV){
-    printf(1, ">>> Informasi direktori dari %s <<<\n", nama);
-    printf(1, "\n");
-    printf(1, "Size\t\t: %d\n", stt.size);
-    printf(1, "Type\t\t: Device\n");
-    printf(1, "Device\t\t: %d\n", stt.dev);
-    printf(1, "File Inode\t: %d\n", stt.ino);
-    printf(1, "Links\t\t: %d\n", stt.nlink);
+  for(i=1; i<argc; i++){
+    info(argv[i]);
   }
 
   exit();
